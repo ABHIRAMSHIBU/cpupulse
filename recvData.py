3007281d870b17b26d503306a1e500ee2555cdc8
Abhiram Shibu <abhiramshibu1998@gmail.com>
2021-08-29 19:23:20 +0530

    cpuplused: Ditch subprocess, use sockets, added download btn
    Co-authored-by: Preet Patel <preet.makz@gmail.com>

--- recvData.py
+++ recvData.py
@@ -1,10 +1,14 @@
-import subprocess as sp
 from pprint import pprint
 import time
 import numpy as np
 from threading import Thread
 import streamlit as st
 import plotly.express as px
+import socket
+from datetime import datetime
+import pandas as pd
+import base64
+import streamlit.components.v1 as components
 
 process = None
 thread = None
@@ -53,54 +57,118 @@ def askData():
     else:
         return None
 
-def setPoll(polling):
-    global pollRate
-    pollRate = polling
-    process.stdin.write(b"pol="+str(polling).encode()+b"\n")
+def askData(host,port):
+    timenow = datetime.now()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(0.05) # Hope optimal time
+        s.connect((host, port))
+        s.send((f"data\n").encode())
+        data=b""
+        while True:
+            try:
+                data += s.recv(1024)
+            except socket.timeout:
+                break
+            if(data==""):
+                break
+        data = data.decode()
 
+        # previndex=0
+        # timestamp=[]
+        # datal=[]
+        # while(True):
+        #     index = data.find("\n",previndex)
+        #     if(index==-1):
+        #         break
+        #     udata = data[previndex:index]
+        #     previndex = index+1
+        #     val = [ j.strip() for j in udata.split(",") ]
+        #     timestamp.append(val[0])
+        #     datal.append(val[1:])
 
-def cacher():
-    global process
-    global thread
-    process = sp.Popen("python3 cpupulse_acquisition_d.py",stdin=sp.PIPE,stdout=sp.PIPE,shell=True)
-    thread = Thread(target=readProcessDataAndQ)
-    thread.start()
-#time.sleep(1)
-cacher()
-# time.sleep(30)
+        data = data.strip().split("\n")
+        timestamp=[]
+        datal=[]
+        for i in data:
+            val = [ j.strip() for j in i.split(",") ]
+            timestamp.append(val[0])
+            datal.append(val[1:])
+
+        timeelapsed = datetime.now() - timenow
+        #pprint(data)
+        # pprint(timeelapsed)
+        # pprint(len(data))
+        s.close()
+        return (np.asarray(datal,dtype=np.float64),np.asarray(timestamp,dtype=np.float64))
+
+def setPoll(polling,host,port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(0.01) # Hope optimal time
+        s.connect((host, port))
+        s.send((f"pol={polling}\n").encode())
+        s.close()
+
+def ts_to_time(ts):
+    if(ts==0):
+        return np.nan
+    return str(datetime.fromtimestamp(float(ts)))
+
+# def cacher():
+#     global process
+#     global thread
+#     thread = Thread(target=readProcessDataAndQ)
+#     thread.start()
+# #time.sleep(1)
+# cacher()
+# # time.sleep(30)
 poll = st.select_slider("Sample Rate",[i for i in range(1,61)])
-zoom_level = st.select_slider("Display Zoomout  ",[i for i in range(1,101,2)]) # Zoom Slider
-data,timestamp = askData()
+zoom_level = (st.select_slider("Display Zoomout  ",[i for i in range(1,1001,2)])/1000) # Zoom Slider
+data,timestamp = askData("localhost",9999)
 cpu = st.select_slider("CPU",[i for i in range(0,len(data[0])+1)])          # CPU Selection
-chart=st.empty()
-samples_persec = sliderToPointsAndSleep(poll)[0]
-# count=0
-try:
-     setPoll(poll)
-     while(True):
-            # try:
-        out = askData()
-        if(out==None):
-            process.stdout.flush()
-            continue
-        data,timestamp = out
-        fig = px.line(data[(samples_persec - int(samples_persec*(zoom_level/100))):,:])
-        print(zoom_level)
-        chart.plotly_chart(fig,use_container_width=True)
+col1, col2 = st.columns(2)
+with col1:
+    plot_bool = st.checkbox("Enable Plot",value=True)
 
-        # except:
-        #     process.stdout.flush()
-        time.sleep(1)
-        # count+=1
-        # if(count==10):
-        #     setPoll(pollRate+1)
+with col2:
+    save_btn = st.button("Save")
+
+chart=st.empty()
+chart_1=st.empty()
+samples_persec = sliderToPointsAndSleep(poll)[0]
+kill_count = 0
+try:
+    setPoll(poll,"localhost",9999)
+    while(True):
+        out = askData("localhost",9999)
+        data,timestamp = out
+        if data.shape[0]!=samples_persec:
+            kill_count+=1
+            print("Something went wrong with SOCKetS!")
+            print(data.shape)
+            if kill_count<=10:
+                continue
+        kill_count=0
+        if(save_btn):
+            save_df = pd.DataFrame(data,index=[ ts_to_time(i) for i in timestamp ])
+            save_df["unixtime"] = timestamp
+            save_df.to_csv("save_data.csv")
+            csv = save_df.to_csv()
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a download=data.csv href="data:file/csv;base64,{b64}">Download csv file</a>'
+            components.html(href)
+
+        if(not plot_bool):
+            break
+        #print(timestamp[(samples_persec - int(samples_persec*(zoom_level/100))):])
+        y=data[(samples_persec - int(samples_persec*(zoom_level))):,:]
+        x=([ ts_to_time(i) for i in timestamp[(samples_persec - int(samples_persec*(zoom_level))):]])
+        y_1=pd.DataFrame(y,index=x)
+        y=pd.DataFrame(y,index=range(len(x))[::-1])
+        fig = px.line(y_1)
+        fig_1 = px.line(y)
+        print(poll)
+        chart.plotly_chart(fig,use_container_width=True)
+        chart_1.plotly_chart(fig_1,use_container_width=True)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     run=False
-    process.stdin.write(b"data\n")
-    process.stdin.flush()
-except:
-    print("RAN")
-    process.kill()
-    raise
-#time.sleep(1000)
-
